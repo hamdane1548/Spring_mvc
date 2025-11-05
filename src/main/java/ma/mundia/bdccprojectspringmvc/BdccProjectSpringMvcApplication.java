ac39aa5e388156566e7fdbe0ba6e78be1096118d
hamdane1548 <oussamahamdane80@gmail.com>
2025-11-05 19:34:02 +0100

    initial commit

---
+++ src/main/java/ma/mundia/bdccprojectspringmvc/BdccProjectSpringMvcApplication.java
@@ -0,0 +1,13 @@
+package ma.mundia.bdccprojectspringmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BdccProjectSpringMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BdccProjectSpringMvcApplication.class, args);
+    }
+
+}

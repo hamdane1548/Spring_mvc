bb3cd10776278059a131f8d4836b11be0019b83d
hamdane1548 <oussamahamdane80@gmail.com>
2025-11-08 15:57:03 +0100

    ajouter produit

--- src/main/java/ma/mundia/bdccprojectspringmvc/web/productControlleur.java
+++ src/main/java/ma/mundia/bdccprojectspringmvc/web/productControlleur.java
@@ -1,11 +1,14 @@
 package ma.mundia.bdccprojectspringmvc.web;
 
+import jakarta.validation.Valid;
 import ma.mundia.bdccprojectspringmvc.Entites.Product;
 import ma.mundia.bdccprojectspringmvc.respostory.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
@@ -14,6 +17,10 @@ import java.util.List;
 public class productControlleur {
     @Autowired
     private ProductRepository productRepository;
+    @GetMapping("")
+    public String home(){
+        return "redirect:/index";
+    }
     @GetMapping("/index")
     public String index(Model model){
         List<Product> produit= productRepository.findAll();
@@ -24,5 +31,22 @@ public class productControlleur {
     public String delete(@RequestParam(name="id") Long id){
      productRepository.deleteById(id);
      return "redirect:/index";
+    }
+    @GetMapping("/createproduit")
+    public String CreateProduit(Model model){
+        model.addAttribute("product", new Product());
+        return "Createproduit";
+    }
+    @PostMapping("/saveproduit")
+    public String saveproduit(@Valid Product product, BindingResult result,Model model){
+       if(result.hasErrors()){
+           System.out.println(result.getAllErrors());
+           System.out.println("ee"+product);
+
+           model.addAttribute("product",product);
+           return "Createproduit";
+       }
+       productRepository.save(product);
+       return "redirect:/index";
     }
 }
